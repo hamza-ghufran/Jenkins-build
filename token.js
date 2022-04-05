65ea245aa681edcd21bf5f38c02cb50d72f2373a
Hamza Ghufran <hamzazebghufran@gmail.com>
2022-04-05 22:05:27 +0530

    feat: setup crumb issuer and gen token handler

---
+++ token.js
@@ -0,0 +1,67 @@
+import 'dotenv/config'
+import axios from 'axios';
+
+const BASE_URL = process.env.BASE_URL
+const USER_NAME = process.env.USER_NAME
+const PASSWORD = process.env.PASSWORD
+
+const api = {
+  crumb: () => `${BASE_URL}/crumbIssuer/api/json`,
+  genToken: (tokenName) => `${BASE_URL}/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken?newTokenName=${tokenName}`
+}
+
+async function getCrumb() {
+  try {
+    const url = api.crumb()
+    const config = {
+      auth: {
+        username: USER_NAME,
+        password: PASSWORD
+      }
+    }
+
+    const resp = await axios.get(url, config)
+
+    return resp
+  }
+  catch (e) {
+    console.log(e);
+  }
+}
+
+async function generateToken(params) {
+  const { crumbIssuer } = params
+
+  const url = api.genToken('testToken');
+
+  try {
+    const res = await axios.post(url, {}, {
+      auth: {
+        username: USER_NAME,
+        password: PASSWORD
+      },
+      headers: {
+        /**
+         * You now have to forward the session id 
+         * (present in the cookie response that generated the crumb) every time you use that crumb. 
+         */
+        Cookie: crumbIssuer.headers['set-cookie'][0],
+        [crumbIssuer.data.crumbRequestField]: crumbIssuer.data.crumb
+      },
+    });
+
+    return res.data
+  } catch (e) {
+    console.log(e)
+  }
+};
+
+async function init() {
+  const crumbIssuer = await getCrumb()
+  const token = await generateToken({ crumbIssuer })
+
+  console.log(token)
+}
+
+
+init().catch((e) => console.error(e))
